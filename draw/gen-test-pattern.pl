7765776d924453895cd6571999e1bdba92cdce39
Jonathan L. Kaus <jlkaus@gmail.com>
2021-07-13 22:04:50 -0500

    Add options to the drawing generator; add a linear quadrature pattern to the test patterns

--- draw/gen-test-pattern.pl
+++ draw/gen-test-pattern.pl
@@ -3,8 +3,21 @@
 use strict;
 use warnings;
 use POSIX "fmod";
+use Getopt::Long;
 
 our $PI = 3.141592653589793;
+
+my $margin = 0;
+my $svg_out = undef;
+my $cfg_in = undef;
+my $page_outline = undef;
+
+GetOptions("m|margin=f" => \$margin,
+           "o|output=s" => \$svg_out,
+           "p|page" => \$page_outline);
+
+$cfg_in = shift || die "ERROR: Must specify a config file\n";
+
 
 
 # generate svg in here for a single page test pattern.
@@ -21,10 +34,6 @@ our $PI = 3.141592653589793;
 #   linear quadrature sequence of various mark widths and heights
 
 my %c = ();
-
-my $cfg_in = shift || die "ERROR: Must specify a config file\n";
-my $svg_out = shift;
-
 open CFGI, "<", $cfg_in or die "ERROR: Can't open $cfg_in for reading\n";
 while(<CFGI>) {
     chomp;
@@ -40,7 +49,7 @@ while(<CFGI>) {
 }
 close(CFGI);
 
-my $doc = createDoc($c{page_height}, $c{page_width}, "Reflective Optical Sensor Test Patterns");
+my $doc = createDoc($c{page_height}, $c{page_width}, $margin, "Reflective Optical Sensor Test Patterns");
 
 foreach(sort keys %c) {
     addChild($doc, createComment("$_: $c{$_}"));
@@ -79,7 +88,7 @@ $h = $c{pat_height};
 my $mw = $c{cr_mark_width};
 my $sw = $c{cr_space_width};
 for(my $cx = 0; $cx < $c{page_width}; $cx += $mw + $sw) {
-    addChild($p, createShape("rect", {x=>$cx, y=>$cy, width=>$mw, height=>$h, fill=>genGrey(0), "stroke-width"=>0}));
+    addChild($p, createShape("rect", {x=>$cx, y=>$cy, width=>$cx + $mw > $c{page_width} ? ($c{page_width} - $cx) : $mw, height=>$h, fill=>genGrey(0), "stroke-width"=>0}));
 }
 $cy += $h;
 
@@ -92,7 +101,7 @@ my $old = $mw;
 my $r = $c{mt_rate};
 $sw = $c{mt_space_width};
 for(my $cx = 0; $cx < $c{page_width}; $cx += $old + $sw) {
-    addChild($p, createShape("rect", {x=>$cx, y=>$cy, width=>$mw, height=>$h, fill=>genGrey(0), "stroke-width"=>0}));
+    addChild($p, createShape("rect", {x=>$cx, y=>$cy, width=>$cx + $mw > $c{page_width} ? ($c{page_width} - $cx) : $mw, height=>$h, fill=>genGrey(0), "stroke-width"=>0}));
     $old = $mw;
     if($c{mt_type} eq "linear") {
         $mw += $r;
@@ -111,7 +120,7 @@ $old = $sw;
 $r = $c{st_rate};
 $mw = $c{st_mark_width};
 for(my $cx = 0; $cx < $c{page_width}; $cx += $old + $mw) {
-    addChild($p, createShape("rect", {x=>$cx, y=>$cy, width=>$mw, height=>$h, fill=>genGrey(0), "stroke-width"=>0}));
+    addChild($p, createShape("rect", {x=>$cx, y=>$cy, width=>$cx + $mw > $c{page_width} ? ($c{page_width} - $cx) : $mw, height=>$h, fill=>genGrey(0), "stroke-width"=>0}));
     $old = $sw;
     if($c{st_type} eq "linear") {
         $sw += $r;
@@ -129,7 +138,7 @@ $w = $c{gt_min};
 $old = $w;
 $r = $c{gt_rate};
 for(my $cx = 0; $cx < $c{page_width}; $cx += 2*$old) {
-    addChild($p, createShape("rect", {x=>$cx, y=>$cy, width=>$w, height=>$h, fill=>genGrey(0), "stroke-width"=>0}));
+    addChild($p, createShape("rect", {x=>$cx, y=>$cy, width=>$cx + $w > $c{page_width} ? ($c{page_width} - $cx) : $w, height=>$h, fill=>genGrey(0), "stroke-width"=>0}));
     $old = $w;
     if($c{gt_type} eq "linear") {
         $w += $r;
@@ -148,7 +157,8 @@ $w = $c{dct_width};
 $mw = $c{dct_min};
 $r = $c{dct_rate};
 for(my $cx = 0; $cx < $c{page_width}; $cx += $w) {
-    addChild($p, createShape("rect", {x=>$cx, y=>$cy, width=>$mw*$w, height=>$h, fill=>genGrey(0), "stroke-width"=>0}));
+    addChild($p, createShape("rect", {x=>$cx, y=>$cy, width=>$cx + $mw*$w > $c{page_width} ? ($c{page_width} - $cx) : $mw*$w,
+                                      height=>$h, fill=>genGrey(0), "stroke-width"=>0}));
     if($c{dct_type} eq "linear") {
         $mw += $r;
     } else {
@@ -157,17 +167,41 @@ for(my $cx = 0; $cx < $c{page_width}; $cx += $w) {
 }
 $cy += $h;
 
-
 # quadrature test patterns
 addChild($pc, createBlank());
 $p = addChild($pc, createGroup("linear_quadrature"));
+$cy += $c{lqp_outer_gap};
+my $ah = $c{lqp_aligner_height};
+my $gh = $c{lqp_gap_height};
+my $ph = $c{lqp_proper_height};
+$sw = $c{lqp_region_width};
+my $xw = $sw/4;
+my $astart = $sw * $c{lqp_align_index};
 
+addChild($p, createShape("rect", {x=>$astart, y=>$cy, width=>$astart + $sw > $c{page_width} ? ($c{page_width} - $astart) : $sw,
+                                  height=>$ah, fill=>genGrey(0), "stroke-width"=>0}));
+$cy += $ah + $gh;
 
+for(my $cx = 0; $cx < $c{page_width}; $cx += $sw) {
+    addChild($p, createShape("rect", {x=>$cx, y=>$cy, width=>$cx + 2* $xw > $c{page_width} ? ($c{page_width} - $cx) : $xw * 2,
+                                      height=>$ph, fill=>genGrey(0), "stroke-width"=>0}));
+    addChild($p, createShape("rect", {x=>$cx + $xw, y=>$cy+$ph, width=>$cx + 3* $xw > $c{page_width} ? ($c{page_width} - $cx - $xw) : $xw * 2,
+                                      height=>$ph, fill=>genGrey(0), "stroke-width"=>0}));
+}
+$cy += 2*$ph + $gh;
+
+addChild($p, createShape("rect", {x=>$astart, y=>$cy, width=>$astart + $sw > $c{page_width} ? ($c{page_width} - $astart) : $sw,
+                                  height=>$ah, fill=>genGrey(0), "stroke-width"=>0}));
 
 
 
 
 addChild($pc, createBlank());
+
+if($page_outline) {
+    # Add a line around the printable pattern area, for reference.
+    addChild($doc, createShape("path",{d=>"M 0 0 L $c{page_width} 0 L $c{page_width} $c{page_height} L 0 $c{page_height} L 0 0", stroke=>"red", fill=>"none", "stroke-width"=>0.1}));
+}
 
 
 open SVGO, ">",$svg_out or die "ERROR: Can't open $svg_out for writing\n" if $svg_out;
@@ -186,10 +220,13 @@ sub genGrey {
 sub blitDoc {
     my ($fh, $doc) = @_;
 
+    my $tw = $doc->{width} + 2*$doc->{margin};
+    my $th = $doc->{height} + 2*$doc->{margin};
+    my $vp = sprintf("%f %f %f %f", -$doc->{margin}, -$doc->{margin}, $tw, $th);
     print $fh "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n";
     #print $fh "<?xml-stylesheet href='$css_out' type='text/css' ?>\n";
     print $fh "<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 2.0//EN\" \"http://www.w3.org/Graphics/SVG/2.0/DTD/svg20.dtd\">\n";
-    print $fh "<svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" height=\"$doc->{height}mm\" width=\"$doc->{width}mm\" viewBox=\"0 0 $doc->{width} $doc->{height}\">\n";
+    print $fh "<svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" height=\"${th}mm\" width=\"${tw}mm\" viewBox=\"${vp}\">\n";
     print $fh "    <title>$doc->{title}</title>\n\n";
 
     foreach(@{$doc->{children}}) {
@@ -236,9 +273,9 @@ sub blitItem {
 
 
 sub createDoc {
-    my ($h, $w, $title) = @_;
+    my ($h, $w, $m, $title) = @_;
 
-    return {height=> $h, width=>$w, title=>$title, children=> []};
+    return {height=> $h, width=>$w, margin=>$m, title=>$title, children=> []};
 }
 
 sub createComment {
